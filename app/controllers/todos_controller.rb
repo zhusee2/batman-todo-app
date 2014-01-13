68b0e04d18d211ead9e4c44023819daab7e204c7
Teng-hao Chang (Zhusee) <zhusee2@gmail.com>
2014-01-13 17:37:15 +0800

    Accept update for todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,12 +2,17 @@ class TodosController < ApplicationController
   respond_to :json
 
   before_action :find_todo_list, :only => [:create]
-  before_action :find_todo,      :only => [:destroy]
+  before_action :find_todo,      :only => [:update, :destroy]
 
   def create
     new_todo = @todo_list.todos.build(todo_params)
     new_todo.save
     respond_with(@todo_list, new_todo)
+  end
+
+  def update
+    @todo.update_attributes(todo_params)
+    respond_with @todo
   end
 
   def destroy
