a553822afe67c8d4eb4e61479225d3a5b385476e
Teng-hao Chang (Zhusee) <zhusee2@gmail.com>
2014-01-13 15:06:02 +0800

    CRUD methods for todos controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,3 +1,26 @@
 class TodosController < ApplicationController
+  respond_to :json
 
+  before_action :find_todo_list, :only => [:create]
+  before_action :find_todo,      :only => [:destroy]
+
+  def create
+    new_todo = @todo_list.todos.build(params[:todo])
+    new_todo.save
+    respond_with(@todo_list, new_todo)
+  end
+
+  def destroy
+    respond_with(@todo.todo_list, @todo.destroy)
+  end
+
+  protected
+
+  def find_todo_list
+    @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
+  def find_todo
+    @todo = Todo.find(params[:id])
+  end
 end
