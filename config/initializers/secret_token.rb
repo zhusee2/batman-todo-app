78e65b6426edbbc412545e3946a00f6b5ba45f5f
Teng-hao Chang (Zhusee) <zhusee2@gmail.com>
2014-01-13 12:36:18 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BatmanTodo::Application.config.secret_key_base = '4aac4f0a5ab505027f8bc16c9784fa4cfa6f9cd4ef79d547fffe35cc596d6834690f10c33c4c764ec40a00c5035537721842675e331ce9ab9a19f0c076983298'
