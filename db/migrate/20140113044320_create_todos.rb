1e8914c68165908f181095f73d5085c142278c20
Teng-hao Chang (Zhusee) <zhusee2@gmail.com>
2014-01-13 13:37:52 +0800

    Fix boolean typo

--- db/migrate/20140113044320_create_todos.rb
+++ db/migrate/20140113044320_create_todos.rb
@@ -2,7 +2,7 @@ class CreateTodos < ActiveRecord::Migration
   def change
     create_table :todos do |t|
       t.string :title
-      t.boolen :completed
+      t.boolean :completed
       t.references :todo_list, index: true
 
       t.timestamps
